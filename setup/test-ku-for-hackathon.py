dc0f107339119d1a2a372c1eb4407543c5aaec81
ethan-pierce <ethan.pierce@colorado.edu>
2020-05-14 13:50:29 -0700

    Fix Ku installation test script

--- setup/test-ku-for-hackathon.py
+++ setup/test-ku-for-hackathon.py
@@ -1,4 +1,7 @@
-import os
+import os, sys
+
+sys.path.append('../permamodel/')
+print('Permamodel added to system path.')
 
 from permamodel.components import bmi_Ku_component
 print('Permamodel is installed.')
@@ -6,12 +9,5 @@ print('Permamodel is installed.')
 cfg_file = os.path.join('./inputs/', 'ku-hackathon-test.cfg')
 x = bmi_Ku_component.BmiKuMethod()
 print('Ku is configured.')
-
-x.initialize(cfg_file)
-print('Ku is initialized')
-
-x.update()
-x.finalize()
-print('Ku successfully ran for 1 time step.')
 
 print('All tests passed.')
